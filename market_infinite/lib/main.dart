fdf1d8465460580b8892c272b2a10368dd85329e
naman <Nmndwdi1001@gmail.com>
2023-07-22 18:07:34 +0530

    signin persistence completed

--- market_infinite/lib/main.dart
+++ market_infinite/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:market_infinite/constants/global_variables.dart';
 import 'package:market_infinite/features/auth/screens/auth_screen.dart';
+import 'package:market_infinite/features/auth/services/auth_service.dart';
+import 'package:market_infinite/features/home/screens/home_screen.dart';
 import 'package:market_infinite/providers/user_provider.dart';
 import 'package:market_infinite/router.dart';
 import 'package:flutter/material.dart';
@@ -13,8 +15,21 @@ void main() {
     child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +49,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const AuthScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty ? const HomeScreen() : const AuthScreen(),
     );
   }
 }
